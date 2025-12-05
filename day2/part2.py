b24885189074d7404012a5e36efbe9e58b1d6cc5
arthur <arthurrrbello@hotmail.com>
2025-12-05 00:06:11 -0300

    finished part2

--- day2/part2.py
+++ day2/part2.py
@@ -1,36 +1,28 @@
+def is_invalid(value: int):
+    value_str = str(value)
+    for i in range(1,len(str(value))//2+1):
+        copy = value_str
+        while copy[:i] == copy[i:2*i]:
+            copy = copy[i:]
+            if len(copy[i:2*i])==0:
+                return True
+
+    return False
+
 def get_invalid_ids(lower:str, upper:str):
-    n_digit_lower = len(lower)
-    n_digit_upper = len(upper)
-
     result = []
+    for x in range(int(lower),int(upper)+1):
+        is_inv = is_invalid(x)
+        if is_inv:
+            result.append(x)
 
-    for n in range(n_digit_lower, n_digit_upper+1):
-        print('--------')
-        print(n)
-        if n%2==0:
-            ranged = 10**(n//2-1)
-            if n_digit_lower%2==0 and ranged < int(lower[:n_digit_lower//2]):
-                ranged = int(lower[:n_digit_lower//2])
-            rangeu = 10**(n//2)
-            if n_digit_upper%2==0 and rangeu > int(upper[:n_digit_upper//2]):
-                rangeu = int(upper[:n_digit_upper//2])+1
-            print('ranged: ', ranged)
-            print('rangeu: ', rangeu)
-            l = [int(str(i)+str(i)) for i in list(range(ranged,rangeu))]
-            if len(l)>=1:
-                if l[0]<int(lower): l.pop(0)
-                if l[-1]> int(upper): l.pop(-1)
-            result = result + l
     return result
 
 
                 
                 
 
-# print(get_invalid_ids('38593856','38593862'))
-# print(get_invalid_ids('1000','8000'))
-print(get_invalid_ids('22951285','23017127'))
-# 22951285-23017127
+
 
 
 result = 0
@@ -44,8 +36,6 @@ with open("day2/input/input.txt", "r") as mfile:
         invalid = get_invalid_ids(str(int(lower)), str(int(upper)))
         print('inv: ', invalid, end=' ')
         print('int: ', interval, end=' ')
-        print('low: ', lower=='22951285', end=' ')
-        print('up: ', upper=='23017127', end=' ')
         print('up: ', str(int(upper)), end=' ')
         result+=sum(invalid)
         print()
