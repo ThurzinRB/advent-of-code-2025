1e69c808ce1c00eda46c90002449c4c5ca5cd190
arthur <arthurrrbello@hotmail.com>
2025-12-08 23:03:28 -0300

    changed parse_input to ignore ids

--- day5/part2.py
+++ day5/part2.py
@@ -7,22 +7,10 @@ def parse_input(file_path:str):
             line_content = line.rstrip()
             if len(line_content) == 0: 
                 is_interval=False
-                continue
-            if is_interval:
-                [low, up] = line_content.split('-')
-                intervals.append([int(low),int(up)])
-            else:
-                ids.append(int(line_content))
-        return [intervals, ids]
+                break
+            [low, up] = line_content.split('-')
+            intervals.append([int(low),int(up)])
+        return intervals
         
-[intervals, ids] = parse_input('day5/input/input.txt')
+intervals = parse_input('day5/input/basic.txt')
 print('intervals: ', intervals)
-print('ids: ', ids)
-
-result = 0
-for id in ids:
-    for interval in intervals:
-        if id>=interval[0] and id<=interval[1]:
-            result+=1
-            break
-print('Result: ', result)
