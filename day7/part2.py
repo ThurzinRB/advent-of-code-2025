16e2f7af77e28c795924bbe56a53566accc55de0
arthur <arthurrrbello@hotmail.com>
2025-12-18 18:19:17 -0300

    removed prints

--- day7/part2.py
+++ day7/part2.py
@@ -21,44 +21,27 @@ with open(file_path, 'r') as mfile:
     data = list(map(lambda x: x.strip('\n'),mfile.readlines()))
 
 
-# print(*data, sep='\n')
 initial_beam = data[0].find('S')
 split_count = 0
 current = [Spot() for i in range(len(data)-1)]
 next = [Spot() for i in range(len(data)-1)]
 
 current[initial_beam] = Spot(True, 1)
-# print(initial_beam)
-# print(data[2][7])
 
 for i in range(len(data)-1):
-    # print('--------')
-    # print(*list(data[i]), sep=' ')
-    # print(*current, sep=', ')
     next = [Spot() for i in range(len(data)-1)]
     for j in range(len(data[i])):
-        # print('\n\ni: ',i, 'j: ', j)
         if current[j].value:
-            # print('current[j].value==True')
-            # print(data[i+1][j])
             if data[i+1][j] == '.':
-                # print('data[i+1][j] != \'^\'')
-                # next[j] = Spot(True, 1)
                 next[j].set_value(True)
                 next[j].add_count(current[j].count)
-                # next[j].se(current[j].count)
-                # print(*next, sep=', ')
             else:
-                # print('data[i+1][j] == \'^\'')
-                # print('current[j]', current[j])
                 split_count+=1
                 next[j].set_value(False)
                 next[j+1].set_value(True)
                 next[j+1].add_count(current[j].count)
                 next[j-1].set_value(True)
                 next[j-1].add_count(current[j].count)
-
-    # print(*next, sep=', ')
             
     print(*[spot.count if spot.count>0 else data[i][k] for k, spot in enumerate(current)])
     current=copy.deepcopy(next)
